3d3a4c427f091d8bb1d932590c6d72f407c67eab
scintillance <meng.r.sun.2010+math@gmail.com>
2026-08-22 18:41:01 -0500

    Made home, blog, and specks pages; updated first lesson with new formatting and an image; updated page layouts to be nested

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,48 +36,63 @@ export default defineConfig({
       rehypePlugins: [
         [rehypeKatex, {
           macros: {
-            "\\th": "\\theta"
+            "\\th": "\\theta",
+            "\\V": "\\bm{\\color{de822b}#1}",
+            "\\B": "\\bm{#1}",
+            "\\I": "\\emph{#1}",
+            "\\Op": "{\\color{73deaa}#1}",
+            "\\Hy": "{\\color{e659a6}#1}",
+            "\\Ad": "{\\color{9480f4}#1}",
+            "\\Opp": "\\text{\\Op{opposite}}",
+            "\\Hyp": "\\text{\\Hy{hypotenuse}}",
+            "\\Adj": "\\text{\\Ad{adjacent}}",
+            "\\Deg": "^\\circ",
           }
         }]
       ],
     }),
   },
   
-  integrations: [
-    AutoImport({
-      imports: [
-        { 
-          './src/components/bits-comps/Horizontal-Bar.astro': [['default', 'Bar']],
-          './src/components/bits-comps/Spacer.astro': [['default', 'Spa']],
+  integrations: [AutoImport({
+    imports: [
+      
+      { 
+        './src/components/bits-comps/Horizontal-Bar.astro': [['default', 'Bar']],
+        './src/components/bits-comps/Spacer.astro': [['default', 'Spa']],
 
-          './src/components/pieces-comps/Right-Triangle-Trinket.astro': [['default', 'RightTriangleTrinket']],
-          './src/components/pieces-comps/Unit-Circle-Trinket.astro': [['default', 'UnitCircleTrinket']],
+        './src/components/pieces-comps/Diagram.astro': [['default', 'Diagram']],
+        './src/components/pieces-comps/Right-Triangle-Trinket.astro': [['default', 'RightTriangleTrinket']],
+        './src/components/pieces-comps/Unit-Circle-Trinket.astro': [['default', 'UnitCircleTrinket']],
 
-          './src/components/text-bits-comps/Tooltip-Challenge.astro': [['default', 'Challenge']],
-          './src/components/text-bits-comps/Tooltip-Formally.astro': [['default', 'Formally']],
-          './src/components/text-bits-comps/Tooltip-Key.astro': [['default', 'Key']],
-          './src/components/text-bits-comps/Tooltip-Note.astro': [['default', 'Note']],
+        './src/components/text-bits-comps/Tooltip-Challenge.astro': [['default', 'Challenge']],
+        './src/components/text-bits-comps/Tooltip-Formally.astro': [['default', 'Formally']],
+        './src/components/text-bits-comps/Tooltip-Key.astro': [['default', 'Key']],
+        './src/components/text-bits-comps/Tooltip-Note.astro': [['default', 'Note']],
 
-          './src/components/text-block-comps/Askew.astro': [['default', 'ASK']],
-          './src/components/text-block-comps/ByTheWay-R.astro': [['default', 'BTW_R']],
-          './src/components/text-block-comps/Dropdown.astro': [['default', 'DRP']],
-          './src/components/text-block-comps/Rectangled.astro': [['default', 'RCT']],
-          './src/components/text-block-comps/Hint.astro': [['default', 'HNT']],
-          './src/components/text-block-comps/Problem.astro': [['default', 'PRB']],
-          './src/components/text-block-comps/Quote.astro': [['default', 'QUO']],
-          './src/components/text-block-comps/Rainbowed.astro': [['default', 'RBW']],
-          './src/components/text-block-comps/Solution.astro': [['default', 'SOL']],
+        './src/components/text-block-comps/Askew.astro': [['default', 'ASK']],
+        './src/components/text-block-comps/Askew2.astro': [['default', 'ASK_II']],
+        './src/components/text-block-comps/ByTheWay-R.astro': [['default', 'BTW_R']],
+        './src/components/text-block-comps/Dropdown.astro': [['default', 'DRP']],
+        './src/components/text-block-comps/Rectangled.astro': [['default', 'RCT']],
+        './src/components/text-block-comps/Hint.astro': [['default', 'HNT']],
+        './src/components/text-block-comps/Problem.astro': [['default', 'PRB']],
+        './src/components/text-block-comps/Quote.astro': [['default', 'QUO']],
+        './src/components/text-block-comps/Rainbowed.astro': [['default', 'RBW']],
+        './src/components/text-block-comps/Solution.astro': [['default', 'SOL']],
 
-          './src/components/text-inline-comps/Pullout.astro': [['default', 'P']],
-          './src/components/text-inline-comps/Subdued.astro': [['default', 'S']],
-          './src/components/text-inline-comps/Tooltip.astro': [['default', 'T']],
-          './src/components/text-inline-comps/Tooltip-new.astro': [['default', 'Tn']],
-          './src/components/text-inline-comps/Vocab.astro': [['default', 'V']],
+        './src/components/text-inline-comps/Pullout.astro': [['default', 'P']],
+        './src/components/text-inline-comps/Start-Italic.astro': [['default', 'Si']],
+        './src/components/text-inline-comps/Start.astro': [['default', 'St']],
+        './src/components/text-inline-comps/Subdued.astro': [['default', 'S']],
+        './src/components/text-inline-comps/Tooltip.astro': [['default', 'T']],
+        './src/components/text-inline-comps/Tooltip-new.astro': [['default', 'Tn']],
+        './src/components/text-inline-comps/Vocab.astro': [['default', 'V']],
 
-        }
-      ],
-    }),
-    
-    mdx(),
-  ],
+        './src/components/text-inline-comps/trig/Adjacent.astro': [['default', "Ad"]],
+        './src/components/text-inline-comps/trig/Opposite.astro': [['default', "Op"]],
+        './src/components/text-inline-comps/trig/Hypotenuse.astro': [['default', "Hy"]],
+
+      }
+    ],
+  }), mdx()],
 });
